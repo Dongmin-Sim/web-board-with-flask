d23161ce334679976cce1a2efcbec18e87d490d8
codongmin <codongmin@gmail.com>
2021-12-12 23:26:51 +0900

    Feat: login, join 화면 구성 변경
    1. login, join 화면 구성 변경
    2. bootstrap 5에 맞는 컴포넌트
    
    - #1

--- app/forms.py
+++ app/forms.py
@@ -1,24 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, EmailField, PasswordField, SelectField
+from wtforms.fields import choices
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
 class JoinForm(FlaskForm):
-    email = EmailField('Email address', validators=[DataRequired(), Email()])
-    user_id = StringField('User Id', validators=[
+    email = EmailField('이메일 주소', validators=[DataRequired(), Email()])
+    user_id = StringField('아이디', validators=[
         DataRequired(), Length(min=8, max=15)])
-    user_pw = PasswordField('User Password', validators=[
+    user_pw = PasswordField('비밀번호', validators=[
         DataRequired(), Length(min=8, max=20), EqualTo('re_password')])
-    re_password = PasswordField('Re Password', validators=[DataRequired(),
+    re_password = PasswordField('비밀번호 확인', validators=[DataRequired(),
                                 Length(min=8, max=20)])
-    interesting = SelectField('interesting', choices=[(
+    interesting = SelectField('관심분야', choices=[(
         'Front-end', 'front-end'), ('Back-end', 'back-end'), ('Data-engineer', 'data-engineer'), ('Product-Manager', 'Product-Manager')])
-    submit = SubmitField('submit')
+    submit = SubmitField('회원가입')
 
 
 class LoginForm(FlaskForm):
-    user_id = StringField('User Id', validators=[
+    user_id = StringField('아이디', validators=[
         DataRequired(), Length(min=8)])
-    user_pw = PasswordField('User Password', validators=[
+    user_pw = PasswordField('비밀번호', validators=[
         DataRequired(), Length(min=8), EqualTo('re_password')])
     submit = SubmitField('submit')
